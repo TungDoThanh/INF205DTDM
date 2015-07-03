f36065400c17b5792c788c0f14d0f8bf938fc904
TungDoThanh <dttung0403@gmail.com>
2015-07-03 22:27:41 +0700

    Create PhepChia.vb

---
+++ PhepChia.vb
@@ -0,0 +1,7 @@
+Public Class PhepCong
+    Public Function PhepChia(ByVal SoA As Integer, ByVal SoB As Integer) As Integer
+        Dim Chia As Integer = 0
+        Chia = SoA / SoB
+        Return Chia
+    End Function
+End Class

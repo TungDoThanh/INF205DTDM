887c7ad93ffd4ed41c853db0db9a21572c62d62a
TungDoThanh <dttung0403@gmail.com>
2015-07-03 22:24:03 +0700

    Create PhepTru.vb

---
+++ PhepTru.vb
@@ -0,0 +1,7 @@
+Public Class PhepTru
+    Public Function PhepCong(ByVal SoA As Integer, ByVal SoB As Integer) As Integer
+        Dim Hieu As Integer = 0
+        Hieu = SoA - SoB
+        Return Hieu
+    End Function
+End Class

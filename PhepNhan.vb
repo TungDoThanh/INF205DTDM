b9686e0fcab7ce07c8d388758ba8f09e15bf395b
TungDoThanh <dttung0403@gmail.com>
2015-07-03 22:24:41 +0700

    Create PhepNhan.vb

---
+++ PhepNhan.vb
@@ -0,0 +1,7 @@
+Public Class PhepNhan
+    Public Function PhepCong(ByVal SoA As Integer, ByVal SoB As Integer) As Integer
+        Dim Nhan As Integer = 0
+        Nhan = SoA * SoB
+        Return Nhan
+    End Function
+End Class
